62887c3e44866bb56228bfdf34e0033b4c0288ca
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-03 03:28:48 +0900

    sommore

--- main.go
+++ main.go
@@ -7,14 +7,20 @@ import (
 )
 
 const (
-	text1 = "Lorem ipsum dolor."
-	text2 = "Lorem dolor sit amet."
+	text1 = `I am the very model of a modern Major-General,
+I've information vegetable, animal, and mineral,
+I know the kings of England, and I quote the fights historical,
+From Marathon to Waterloo, in order categorical.`
+	text2 = `I am the very model of a cartoon individual,
+My animation's comical, unusual, and whimsical,
+I'm quite adept at funny gags, comedic theory I have read,
+From wicked puns and stupid jokes to anvils that drop on your head.`
 )
 
 func main() {
 	dmp := diffmatchpatch.New()
 
-	diffs := dmp.DiffMain(text1, text2, false)
+	diffs := dmp.DiffMain(text1, text2, true)
 
 	patch := `@@ -54,5 +54,8 @@
          </div>
@@ -37,5 +43,22 @@ func main() {
 	for _, p := range patches {
 		fmt.Println("p:", p.String())
 	}
+
+	textA := `FROM node:18-alpine
+WORKDIR /app
+ADD package.json /app 
+RUN npm i --silent
+ADD . /app 
+CMD npm run start`
+
+	textB := `
+FROM node:18-alpine
+WORKDIR /src
+ADD package.json /src 
+RUN npm i --silent
+ADD . /src 
+CMD npm start`
+	diffs = dmp.DiffMain(textA, textB, true)
+	fmt.Println("DiffPrettyText:", dmp.DiffPrettyText(diffs))
 
 }
