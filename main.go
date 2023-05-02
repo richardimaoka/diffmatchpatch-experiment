1913a94adc66cb79f05040f873667724ca43de92
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-03 02:41:34 +0900

    extra stuff

--- main.go
+++ main.go
@@ -16,5 +16,26 @@ func main() {
 
 	diffs := dmp.DiffMain(text1, text2, false)
 
-	fmt.Println(dmp.DiffPrettyText(diffs))
+	patch := `@@ -54,5 +54,8 @@
+         </div>
+       </div>
+     </div>
++    <button>1</button>
++    <button>2</button>
++    <button>3</button>
+   </body>
+ </html>`
+
+	fmt.Println("DiffPrettyText:", dmp.DiffPrettyText(diffs))
+	fmt.Println("DiffText1     :", dmp.DiffText1(diffs))
+	fmt.Println("DiffText2     :", dmp.DiffText2(diffs))
+
+	patches, err := dmp.PatchFromText(patch)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, p := range patches {
+		fmt.Println("p:", p.String())
+	}
+
 }
